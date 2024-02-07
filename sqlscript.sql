72275c33f9499699ba395f2a63d7ba38c9c137fe
rohit1381999 <124949699+rohit1381999@users.noreply.github.com>
2024-02-07 23:12:06 +0530

    Weather_Data_Analysis.sql

--- sqlscript.sql
+++ sqlscript.sql
@@ -1,9 +1,10 @@
 REM   Script: SQL MAJOR ASSIGNMENT0405
 REM   SQL MAJOR ASSIGNMENT0405
-
+/*Question-->1
+Create a table “Station” to store information about weather
+observation stations*/
 CREATE TABLE STATION 
-( 
-  ID NUMBER(7), 
+( ID NUMBER(7), 
   CITY CHAR(20), 
   STATE CHAR(2), 
   LAT_N  NUMBER(10), 
@@ -11,6 +12,10 @@ CREATE TABLE STATION
   PRIMARY KEY(ID) 
 );
 
+/*Question-->2
+Insert the following records into the table:
+note: {Tables mention in the attached pdf file please visit this}
+*/
 INSERT INTO STATION VALUES 
 (13, 'PHOENIX', 'AZ', 33, 112);
 
@@ -20,12 +25,16 @@ INSERT INTO STATION VALUES
 INSERT INTO STATION VALUES 
 (66, 'CARIBOU', 'ME', 47, 68);
 
-SELECT * FROM STATION;
-
+/** Question-->4
+Execute a query to select Northern stations (Northern latitude >
+39.7) **/
 SELECT * FROM STATION 
 WHERE (LAT_N>39.7) 
 ORDER BY LAT_N DESC;
 
+/** Question-->5
+Create another table, ‘STATS’, to store normalized temperature and
+precipitation data: (mention in Attached Pdf files) **/
 CREATE TABLE STATS 
 ( 
 ID NUMBER(7), 
@@ -35,6 +44,8 @@ RAIN_I NUMBER (6,3),
 PRIMARY KEY (ID, MONTH) 
 );
 
+/** Questions-->6
+Populate the table STATS with some statistics for January and July **/
 INSERT INTO STATS VALUES 
 (13,1,57.4,.31);
 
@@ -53,52 +64,29 @@ INSERT INTO STATS VALUES
 INSERT INTO STATS VALUES 
 (66,7,65.8,4.52);
 
+
+/* Question-->7
+Execute a query to display temperature stats (from STATS table) for
+each city (from Station table)*/
 SELECT CITY, TEMP_F FROM M STATION 
 INNER JOIN STATS 
 ON STATION.ID=STATS.ID;
 
-SELECT CITY, TEMP_F FROM  STATION 
-INNER JOIN STATS 
-ON STATION.ID=STATS.ID;
 
-ON STATION.ID=STATS.ID;*// 
-
-
+/* Question--> 8
+Execute a query to look at the table STATS, ordered by month and
+greatest rainfall, with columns rearranged. It should also show the
+corresponding cities*/
 SELECT MONTH, RAIN_I, CITY 
 FROM STATS 
 INNER JOIN STATION 
 ON STATS.ID = STATION.ID 
 ORDER BY MONTH, RAIN_I DESC;
+;
 
-SELECT MONTH, RAIN_I, CITY 
-FROM STATS 
-INNER JOIN STATION 
-ON STATS.ID = STATION.ID 
-ORDER BY MONTH, RAIN_I DESC;
-
-SELECT MONTH, RAIN_I, CITY 
-FROM STATS 
-INNER JOIN STATION 
-ON STATS.ID = STATION.ID 
-ORDER BY MONTH, RAIN_I ASC;
-
-SELECT MONTH, RAIN_I, CITY 
-FROM STATS 
-INNER JOIN STATION 
-ON STATS.ID = STATION.ID 
-ORDER BY MONTH, RAIN_I DESC;
-
-SELECT MONTH, RAIN_I, CITY 
-FROM STATS 
-INNER JOIN STATION 
-ON STATS.ID = STATION.ID 
-ORDER BY MONTH, RAIN_I ASC;
-
-SELECT MONTH, RAIN_I, CITY 
-FROM STATS 
-INNER JOIN STATION 
-ON STATS.ID = STATION.ID 
-ORDER BY MONTH, RAIN_I DESC;
+/**Question-->9
+Execute a query to look at temperatures for July from table STATS,
+lowest temperatures first, picking up city name and latitude **/
 
 SELECT CITY, LAT_N AS LATITUDE, TEMP_F AS TEMPERATURE, MONTH, FROM STATION 
 INNER JOIN STATS 
@@ -106,18 +94,10 @@ ON STATION.ID = STATS.ID
 WHERE MONTH=7 
 ORDER BY TEMP_F;
 
-SELECT CITY, LAT_N AS LATITUDE, TEMP_F AS TEMPERATURE, MONTH, FROM STATION 
-INNER JOIN STATS 
-ON STATION.ID = STATS.ID 
-WHERE MONTH=7 
-ORDER BY TEMP_F;
 
-SELECT CITY, LAT_N AS LATITUDE, TEMP_F AS TEMPERATURE, MONTH  
-FROM STATION 
-INNER JOIN STATS 
-ON STATION.ID = STATS.ID 
-WHERE MONTH=7 
-ORDER BY TEMP_F;
+/**Question-->10
+Execute a query to show MAX and MIN temperatures as well as
+average rainfall for each city. **/
 
 SELECT CITY, MAX(TEMP_F) AS MAX_TEMP, MIN(TEMP_F) AS MIN_TEMP, AVG(RAIN_I) AS RAINFALL FROM STATION 
 INNER JOIN STATS 
@@ -128,11 +108,17 @@ CREATE VIEW STATS_B (ID, MONTH, TEMP_C, RAIN_CM) AS
 SELECT ID, MONTH, (TEMP_F-32)*5/9, RAIN_I*2.54 
 FROM STATS;
 
+
+/** Question-->12
+Update all rows of table STATS to compensate for faulty rain gauges
+known to read 0.01 inches low **/
 UPDATE STATS SET RAIN_I = RAIN_I + 0.01;
 
 UPDATE STATS SET TEMP_F=74.9 
 WHERE ID=44 AND MONTH=7;
 
+/** Question-->13
+Update Denver's July temperature reading as 74.9 **/
 SELECT * FROM STATS 
 /*UPDATE STATS SET TEMP_F=74.9 
 WHERE ID=44 AND MONTH=7;*/ 
@@ -141,24 +127,9 @@ WHERE ID=44 AND MONTH=7;*/
 
 SELECT * FROM STATS 
 /*UPDATE STATS SET TEMP_F=74.9 
-WHERE ID=44 AND MONTH=7;*/ 
+WHERE ID=44 AND MONTH=7; */ 
  
-;
 
-SELECT * FROM STATS 
- 
-;
+**************Thank You**************
 
-UPDATE STATS SET TEMP_F=74.9 
-WHERE ID=44 AND MONTH=7;
-
-UPDATE STATS SET TEMP_F=74.9 
-WHERE ID=44 AND MONTH=7;
-
-SELECT * FROM STATS;
-
-UPDATE STATS SET TEMP_F=74.9 
-WHERE ID=44 AND MONTH=7;
-
-SELECT * FROM STATS;
 
